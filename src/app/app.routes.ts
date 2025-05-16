47f8c504cc9fc7473a4172b2cd5d1d480be89cbb
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2025-05-16 18:02:11 +0700

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+// src/app/app.routes.ts
+import { Routes } from '@angular/router';
+import { FormComponent } from './pages/form/form.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: FormComponent,
+  },
+];
